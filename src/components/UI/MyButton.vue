ff9f9d141549c9b77d43548cae1abe6bc46b3d47
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2026-01-08 22:01:15 +0700

    добавлен компонент MyButton

---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button class="button" :class="{ 'button--filled': filled }">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+defineOptions({
+  name: "my-button",
+});
+const props = defineProps({
+  filled: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="less">
+.button {
+  padding: 16px 32px;
+  border-radius: 16px;
+  border: 1px solid @gray-2;
+  background-color: @light;
+
+  &:hover {
+    background-color: @gray-1;
+  }
+
+  &--filled {
+    border: 1px solid @purple;
+    background-color: @purple;
+    color: @light;
+
+    &:hover {
+      background-color: @purple-1;
+    }
+  }
+}
+</style>
